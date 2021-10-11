d48e9baa98f88fbe6390550554f51cd6ca7e67ad
Trieu Than <trieuthan92pro@gmail.com>
2021-10-11 17:15:26 +0700

    Add ex 4-6

---
+++ Lesson 2.5/Exercises5.cpp
@@ -0,0 +1,30 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.10
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int countDigits(long long n) {
+    if (n < 10) { // điều kiện dừng
+        return 1;
+    }
+    else { // bước đệ quy
+        return 1 + countDigits(n / 10);
+    }
+}
+
+int main() {
+    long long n;
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+    if (n > 0) {
+        cout << countDigits(n) << endl;
+    }
+    else {
+        cout << "Nhap n > 0.\n";
+    }
+    return 0;
+}
---
+++ Lesson 2.5/Exercises6.cpp
@@ -0,0 +1,30 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.10
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int firstDigit(long long n) {
+    if (n < 10) { // điểm dừng
+        return n;
+    }
+    else { // bước đệ quy
+        return firstDigit(n / 10);
+    }
+}
+
+int main() {
+    long long n;
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+    if (n > 0) {
+        cout << firstDigit(n) << endl;
+    }
+    else {
+        cout << "Nhap n > 0.\n";
+    }
+    return 0;
+}
