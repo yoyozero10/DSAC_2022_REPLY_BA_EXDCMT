4b1a57724fae12a35bdd761ec56312fa8ec806b8
Trieu Than <trieuthan92pro@gmail.com>
2021-10-11 16:21:22 +0700

    Add exercises 6 l2.3

---
+++ Lesson 2.3/Exercises6.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+void showRect(string** rect, int m, int n) {
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << rect[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void drawRect(int m, int n) {
+	string** rect = new string * [m];
+	for (int i = 0; i < m; i++)
+	{
+		rect[i] = new string[n];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			// chỉ vẽ dấu * ở viền ngoài
+			if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+				rect[i][j] = " * ";
+			}
+			else { // còn lại là dấu cách
+				rect[i][j] = "   ";
+			}
+		}
+	}
+	showRect(rect, m, n);
+	for (int i = 0; i < m; i++)
+	{
+		delete[] rect[i];
+	}
+	delete[] rect;
+}
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+	if (m > 0 && n > 0) {
+		drawRect(m, n);
+	}
+	else {
+		cout << "ERROR\n";
+	}
+}
