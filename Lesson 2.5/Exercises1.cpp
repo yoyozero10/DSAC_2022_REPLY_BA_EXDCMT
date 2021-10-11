b0fc7f37ffc80216144059113cb193fcbf128f60
Trieu Than <trieuthan92pro@gmail.com>
2021-10-11 17:00:13 +0700

    Add ex1-3 l2.5

---
+++ Lesson 2.5/Exercises1.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.10
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// hàm tính giai thừa bằng đệ quy
+unsigned long long factorial(int n) {
+    if (n < 2) { // điểm dừng
+        return 1;
+    }
+    else { // bước đệ quy
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Nhap n khong qua 22: ";
+    cin >> n;
+    if (n > 0 && n <= 22) {
+        cout << factorial(n) << endl;
+    }
+    else {
+        cout << "Gia tri n khong hop le.\n";
+    }
+}
---
+++ Lesson 2.5/Exercises2.cpp
@@ -0,0 +1,30 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.10
+ *
+ */
+#include <iostream>
+using namespace std;
+
+unsigned long long fiboN(int n) {
+    if (n < 2) { // điểm dừng
+        return n;
+    }
+    else { // bước đệ quy
+        return fiboN(n - 1) + fiboN(n - 2);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Nhap n <= 90: ";
+    cin >> n;
+    if (n >= 0 && n <= 90) {
+        cout << "F(" << n << ") = " << fiboN(n) << endl;
+    }
+    else {
+        cout << "Nhap n <= 90.\n";
+    }
+    return 0;
+}
