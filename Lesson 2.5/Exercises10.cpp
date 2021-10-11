c1ec3780fd4318c153aa7be2a18d79d52ec5d714
Trieu Than <trieuthan92pro@gmail.com>
2021-10-11 18:05:22 +0700

    Add ex9-12 l2.5

---
+++ Lesson 2.5/Exercises10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+double add(int n) {
+    if (n == 1) { // điểm dừng
+        return 1;
+    }
+    else { // bước đệ quy
+        return 1.0 / n + add(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Nhap so nguyen duong n: ";
+    cin >> n;
+    if (n > 0) {
+        cout << "SUM = " << add(n) << endl;
+    }
+    else {
+        cout << "Nhap vao hai so nguyen duong.\n";
+    }
+    return 0;
+}
