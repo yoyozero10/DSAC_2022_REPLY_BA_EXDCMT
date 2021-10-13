3150866d317a862300f5ecc7b8073e5d1fff73ca
Trieu Than <trieuthan92pro@gmail.com>
2021-10-13 12:51:17 +0700

    Add ex1-4 l2.10

---
+++ Lesson 2.10/Exercises1.cpp
@@ -0,0 +1,57 @@
+﻿/**
+ * @author Branium Academy
+ * @version 2021.10
+ * @see <a href="https://braniumacademy.net/">...</a>
+ */
+#include <iostream>
+using namespace std;
+
+// khai báo nguyên mẫu hàm
+void generate(int* arr, int n); 
+bool nextBinaryString(int* arr, int n); 
+void output(int* arr, int n); 
+
+void generate(int* arr, int n) { // thuật toán sinh tất cả các xâu
+	bool isFinal = false;
+	while (!isFinal) {
+		output(arr, n);
+		isFinal = nextBinaryString(arr, n);
+	}
+}
+
+bool nextBinaryString(int* arr, int n) { // thuật toán sinh xâu kế tiếp
+	int i = n - 1;
+	while (i >= 0 && arr[i] != 0) {
+		arr[i] = 0;
+		i--;
+	}
+	if (i >= 0) {
+		arr[i] = 1;
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+void output(int* arr, int n) { // hiển thị kết quả
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int t;
+	int count = 1;
+	cin >> t;
+	while (t-- > 0) {
+		int n;
+		cin >> n;
+		cout << "Test " << count++ << ": \n";
+		int* arr = new int[n]();
+		generate(arr, n);
+		delete[] arr;
+	}
+}
---
+++ Lesson 2.10/Exercises4.cpp
@@ -0,0 +1,44 @@
+﻿/**
+ * @author Branium Academy
+ * @version 2021.10
+ * @see <a href="https://braniumacademy.net/">...</a>
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+/**
+ * Phương thức chuyển đổi chuỗi nhị phân sang giá trị số ở hệ 10
+ *
+ * @param arr mảng chứa chuỗi nhị phân
+ * @return giá trị số tính được từ chuỗi nhị phân cho trước ở hệ 10
+ */
+int convertBinaryToDecimal(int* arr, int n) {
+    int number = 0;
+    for (int i = 0; i < n; i++) {
+        number += arr[i] * (int)pow(2, n - 1 - i);
+    }
+    return number;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    int count = 1;
+    while (t-- > 0) {
+        string testCase;
+        getline(cin, testCase);
+        stringstream ss(testCase);
+        int n = 0;
+        int arr[100];
+        int x;
+        while (ss >> x) {
+            arr[n++] = x;
+        }
+        int result = convertBinaryToDecimal(arr, n);
+        cout << result << endl;
+    }
+	return 0;
+}
