97c297db1693de74f6171b7ce349b826011abfd8
Trieu Than <trieuthan92pro@gmail.com>
2021-10-31 17:05:17 +0700

    Add ex1 l5.6

---
+++ Lesson5.6/Exercises1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+bool findX(queue<string> friends, string x) {
+    while (!friends.empty()) {
+        if (friends.front().compare(x) == 0) {
+            return true;
+        }
+        friends.pop();
+    }
+    return false;
+}
+
+int main()
+{
+    queue<string> friends;
+    // thêm phần tử mới vào hàng đợi
+    friends.push("Huong");
+    friends.push("Hoa");
+    friends.push("Hien");
+    friends.push("Ha");
+    friends.push("Thanh");
+    // kiểm tra hàng đợi có rỗng không
+    cout << "Hang doi rong? " << boolalpha << friends.empty() << endl;
+    // cho biết số phần tử hiện có trong hàng đợi
+    cout << "So phan tu hien co trong hang doi: " << friends.size() << endl;
+    // lấy phần tử đầu hàng đợi
+    cout << "Phan tu dau hang doi: " << friends.front() << endl;
+    // lấy phần tử cuối hàng đợi
+    cout << "Phan tu cuoi hang doi: " << friends.back() << endl;
+    // xóa phần tử đầu hàng đợi
+    friends.pop(); // done
+    // tìm kiếm phần tử x
+    string x = "Mai";
+    cout << x << " co ton tai trong hang doi? " << findX(friends, x) << endl;
+    // hiển thị các phần tử hiện có trong hàng đợi
+    while (!friends.empty()) {
+        cout << friends.front() << "->";
+        friends.pop();
+    }
+    cout << "null\n";
+    // the end
+    return 0;
+}
