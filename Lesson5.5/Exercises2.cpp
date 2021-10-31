4dd9e65a008f5ae88e0130f88680f57fb88347e5
Trieu Than <trieuthan92pro@gmail.com>
2021-10-31 16:22:29 +0700

    Add ex1-3 lesson 5.5

--- Lesson5.5/Exercises2.cpp
+++ Lesson5.5/Exercises2.cpp
@@ -8,96 +8,87 @@
 #include <exception>
 using namespace std;
 
-template<class T> class Node {
-public:
-	T data;
-	int priority; // thứ tự ưu tiên
-	Node<T>* next;
 
-	Node(T data, int priority) {
-		this->priority = priority;
-		this->data = data;
-		this->next = nullptr;
-	}
-};
-
-
-template<class T> class PriorityQueue {
+// quy ước phần tử có giá trị lớn hơn thì mức ưu tiên cao hơn
+class PriorityQueue {
 private:
-	Node<T>* head;
-	Node<T>* tail;
+	long* data;
 	int currentSize;
+	int capacity;
 public:
-	PriorityQueue() {
+	PriorityQueue(int cap = 10) {
 		currentSize = 0;
-		head = nullptr;
-		tail = nullptr;
+		capacity = cap;
+		data = new long[capacity];
 	}
 	// kiểm tra hàng đợi rỗng
 	bool isEmpty() {
-		return head == nullptr;
+		return currentSize == 0;
+	}
+	// kiểm tra hàng đợi đầy
+	bool isFull() {
+		return currentSize == capacity;
 	}
 	// thêm phần tử vào cuối hàng đợi
-	bool push(T data, int priority) {
-		Node<T>* p = new Node<T>(data, priority);
-		if (p == nullptr) {
+	bool push(long value) {
+		if (isFull()) {
 			return false;
 		}
 		if (isEmpty()) {
-			head = tail = p;
+			data[currentSize++] = value;
 		}
-		else if (priority > head->priority) {
-			p->next = head;
-			head = p;
+		else if (value < data[currentSize - 1]) {
+			data[currentSize++] = value;
 		}
 		else {
-			Node<T>* r = head;
-			for (Node<T>* q = head->next; q != nullptr; q = q->next)
+			int rightPosition = 0;
+			// tìm vị trí chính xác của value
+			for (int i = 0; i < currentSize; i++)
 			{
-				if (q->priority < priority) {
+				if (data[i] <= value) {
+					rightPosition = i;
 					break;
 				}
-				r = q;
 			}
-			p->next = r->next;
-			r->next = p;
-			if (r == tail) {
-				tail = p;
+			for (int i = currentSize; i > rightPosition; i--) {
+				data[i] = data[i - 1];
 			}
+			data[rightPosition] = value;
+			currentSize++;
 		}
-		currentSize++;
 		return true;
 	}
 	// lấy phần tử đầu hàng đợi
-	Node<T> front() {
+	long front() {
 		if (isEmpty()) {
 			throw exception("Queue rong.");
 		}
 		else {
-			return *head;
+			return data[0];
 		}
 	}
 	// lấy phần tử cuối hàng đợi
-	Node<T> back() {
+	long back() {
 		if (isEmpty()) {
 			throw exception("Queue rong.");
 		}
 		else {
-			return *tail;
+			return data[currentSize - 1];
 		}
 	}
 	// xóa và trả về phần tử đầu hàng đợi
-	Node<T> pop() {
+	long pop() {
 		if (isEmpty()) {
 			throw exception("Queue rong.");
 		}
 		else {
-			Node<T>* r = head;
-			head = head->next;
+			long frontValue = data[0];
+			for (int i = 0; i < currentSize - 1; i++)
+			{
+				data[i] = data[i + 1];
+			}
 			currentSize--;
-			Node<T> p = *r;
-			delete r;
-			return p;
+			return frontValue;
 		}
 	}
 	// trả về số phần tử hiện thời trong hàng đợi
@@ -110,29 +101,34 @@ public:
 			cout << "Queue rong.\n";
 		}
 		else {
-			int minPriority = tail->priority;
-			cout << "Muc uu tien: " << minPriority << endl;
-			cout << "Danh sach phan tu co muc uu tien nho nhat: \n";
-			for (Node<T>* p = head; p != nullptr; p = p->next)
+			cout << "Vi tri phan tu dau tien co muc uu tien nho nhat: ";
+			long minPriorityValue = data[currentSize - 1];
+			for (int i = 0; i < currentSize; i++)
 			{
-				if (p->priority == minPriority) {
-					cout << p->data << " ";
+				if (data[i] == minPriorityValue) {
+					cout << i << endl;
+					break;
 				}
 			}
-			cout << endl;
 		}
 	}
 	// hiển thị các phần tử cùng với mức ưu tiên của nó
 	void printElements() {
-		for (Node<T>* p = head; p != nullptr; p = p->next)
+		for (int i = 0; i < currentSize; i++)
 		{
-			cout << "[" << p->data << " - " << p->priority << "]\n";
+			cout << data[i] << " ";
 		}
+		cout << endl;
+	}
+	// hàm hủy
+	~PriorityQueue() {
+		delete[] data;
 	}
 };
 
 int main() {
-	PriorityQueue<int> queue;
+	// khai báo queue có tối đa 100 phần tử
+	PriorityQueue queue(100);
 	int choice;
 	do
 	{
@@ -144,7 +140,8 @@ int main() {
 		cout << "5. Lay so phan tu hien co trong hang doi.\n";
 		cout << "6. Hien thi cac phan tu cua hang doi.\n";
 		cout << "7. Kiem tra queue rong khong.\n";
-		cout << "8. Liet ke cac phan tu co muc uu tien nho nhat.\n";
+		cout << "8. Kiem tra queue day chua.\n";
+		cout << "9. Tim chi so phan tu dau tien co muc uu tien nho nhat.\n";
 		cout << "0. Thoat chuong trinh.\n";
 		cout << "Lua chon cua ban? ";
 		cin >> choice;
@@ -155,18 +152,16 @@ int main() {
 			break;
 		case 1:
 		{
-			int x, priority;
+			int x;
 			cout << "Nhap gia tri can them moi: ";
 			cin >> x;
-			cout << "Nhap thu tu uu tien: ";
-			cin >> priority;
-			queue.push(x, priority);
+			queue.push(x);
 			break;
 		}
 		case 2: 
 		{
 			try {
-				cout << "Phan tu vua xoa: " << queue.pop().data << endl;
+				cout << "Phan tu vua xoa: " << queue.pop() << endl;
 			}
 			catch (exception e) {
 				cout << e.what() << endl;
@@ -176,7 +171,7 @@ int main() {
 		case 3:
 		{
 			try {
-				cout << "Phan tu dau hang doi: " << queue.front().data << endl;
+				cout << "Phan tu dau hang doi: " << queue.front() << endl;
 			}
 			catch (exception e) {
 				cout << e.what() << endl;
@@ -186,7 +181,7 @@ int main() {
 		case 4:
 		{
 			try {
-				cout << "Phan tu cuoi hang doi: " << queue.back().data << endl;
+				cout << "Phan tu cuoi hang doi: " << queue.back() << endl;
 			}
 			catch (exception e) {
 				cout << e.what() << endl;
@@ -209,6 +204,9 @@ int main() {
 			cout << "Queue rong? " << boolalpha << queue.isEmpty() << endl;
 			break;
 		case 8:
+			cout << "Queue day? " << boolalpha << queue.isFull() << endl;
+			break;
+		case 9:
 			if (queue.isEmpty()) {
 				cout << "Queue rong.\n";
 			}
---
+++ Lesson5.5/Exercises3.cpp
@@ -0,0 +1,103 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.10
+ *
+ */
+#include <iostream>
+using namespace std;
+
+class PriorityQueue {
+private:
+	long* data;
+	int currentSize;
+	int capacity;
+public:
+	PriorityQueue(int cap = 10) {
+		currentSize = 0;
+		capacity = cap;
+		data = new long[capacity];
+	}
+	// kiểm tra hàng đợi rỗng
+	bool isEmpty() {
+		return currentSize == 0;
+	}
+	// kiểm tra hàng đợi đầy
+	bool isFull() {
+		return currentSize == capacity;
+	}
+	// thêm phần tử vào cuối hàng đợi
+	bool push(long value) {
+		if (isFull()) {
+			return false;
+		}
+		if (isEmpty()) {
+			data[currentSize++] = value;
+		}
+		else if (value < data[currentSize - 1]) {
+			data[currentSize++] = value;
+		}
+		else {
+			int rightPosition = 0;
+			// tìm vị trí chính xác của value
+			for (int i = 0; i < currentSize; i++)
+			{
+				if (data[i] <= value) {
+					rightPosition = i;
+					break;
+				}
+			}
+			for (int i = currentSize; i > rightPosition; i--) {
+				data[i] = data[i - 1];
+			}
+			data[rightPosition] = value;
+			currentSize++;
+		}
+		return true;
+	}
+	// lấy phần tử đầu hàng đợi
+	long front() {
+		return data[0];
+	}
+	// lấy phần tử cuối hàng đợi
+	long back() {
+		return data[currentSize - 1];
+	}
+	// xóa và trả về phần tử đầu hàng đợi
+	void pop() {
+		for (int i = 0; i < currentSize - 1; i++)
+		{
+			data[i] = data[i + 1];
+		}
+		currentSize--;
+	}
+	// trả về số phần tử hiện thời trong hàng đợi
+	int size() {
+		return currentSize;
+	}
+	// hàm hủy
+	~PriorityQueue() {
+		delete[] data;
+	}
+};
+
+int main()
+{
+	int n;
+	cin >> n;
+	int max = n;
+	PriorityQueue* queue = new PriorityQueue(n);
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		queue->push(x);
+		while (!queue->isEmpty() && queue->front() == max) {
+			cout << queue->front() << ' ';
+			queue->pop();
+			--max;
+		}
+		cout << endl;
+	}
+	delete queue;
+	return 0;
+}
