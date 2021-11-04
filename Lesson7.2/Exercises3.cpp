d742b21097f29007175ce856efcd8d0c3cfe639d
Trieu Than <trieuthan92pro@gmail.com>
2021-11-04 16:31:35 +0700

    Add ex1-4 l7.2

---
+++ Lesson7.2/Exercises3.cpp
@@ -0,0 +1,143 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.11
+ *
+ */
+#include <iostream>
+using namespace std;
+
+template<class T> class Heap {
+	T* data;
+	int currentSize;
+	int capacity;
+public:
+	Heap(int cap = 10) {
+		if (cap <= 0) {
+			capacity = 10;
+		}
+		else {
+			capacity = cap;
+		}
+		currentSize = 0;
+		data = new T[capacity];
+	}
+
+	bool add(T value) {
+		if (currentSize < capacity) {
+			data[currentSize] = value;
+			siftUp(currentSize);
+			currentSize++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void siftUp(int index) {
+		int parentIndex = (index - 1) / 2;
+		if (data[index] < data[parentIndex]) {
+			swap(data[index], data[parentIndex]);
+			siftUp(parentIndex);
+		}
+	}
+
+	void showElements() {
+		if (isEmpty()) {
+			cout << "Heap rong.\n";
+		}
+		else {
+			for (int i = 0; i < currentSize; i++)
+			{
+				cout << data[i] << ' ';
+			}
+			cout << endl;
+		}
+	}
+
+	// phần tử có giá trị nhỏ nhất trong heap
+	T const front() {
+		if (currentSize >= 0) {
+			return data[0];
+		}
+		else {
+			return T();
+		}
+	}
+
+	int size() {
+		return currentSize;
+	}
+
+	bool isEmpty() {
+		return currentSize == 0;
+	}
+
+	~Heap() {
+		delete[] data;
+	}
+};
+
+int main() {
+	Heap<int> heap;
+	int choice;
+	do
+	{
+		cout << "================ OPTIONS ================\n";
+		cout << "1. Them moi mot node.\n";
+		cout << "2. Hien thi cac phan tu trong heap.\n";
+		cout << "3. Cho biet phan tu nho nhat trong heap.\n";
+		cout << "4. Lay kich thuoc hien tai cua heap.\n";
+		cout << "0. Thoat chuong trinh.\n";
+		cout << "Lua chon cua ban? ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			cout << "<== The End ==>\n";
+			break;
+		case 1:
+		{
+			int x;
+			cout << "Nhap x = ";
+			cin >> x;
+			heap.add(x);
+			break;
+		}
+		case 2:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Cac phan tu trong heap: \n";
+				heap.showElements();
+			}
+			break;
+		case 3:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Phan tu lon nhat trong heap: " << heap.front() << endl;
+			}
+			break;
+		case 4:
+			cout << "Kich thuoc hien thoi cua heap: " << heap.size() << endl;
+			break;
+		default:
+			cout << "Sai lua chon. Vui long thu lai.\n";
+			break;
+		}
+	} while (choice != 0);
+
+	return 0;
+}
+// ví dụ một max heap:
+//             90
+//           /    \
+//		   70     80
+//        / \    /  \
+//       40 30  10  50
+//      / \
+//     15  20
---
+++ Lesson7.2/Exercises4.cpp
@@ -0,0 +1,180 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.11
+ *
+ */
+#include <iostream>
+#include <exception>
+using namespace std;
+
+template<class T> class PriorityQueue {
+	T* data;
+	int currentSize;
+	int capacity;
+public:
+	PriorityQueue(int cap = 10) {
+		if (cap <= 0) {
+			capacity = 10;
+		}
+		else {
+			capacity = cap;
+		}
+		currentSize = 0;
+		data = new T[capacity];
+	}
+
+	bool push(T value) {
+		if (currentSize < capacity) {
+			data[currentSize] = value;
+			siftUp(currentSize);
+			currentSize++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void siftUp(int index) {
+		int parentIndex = (index - 1) / 2;
+		if (data[index] < data[parentIndex]) {
+			swap(data[index], data[parentIndex]);
+			siftUp(parentIndex);
+		}
+	}
+
+	void showElements() {
+		if (isEmpty()) {
+			cout << "Heap rong.\n";
+		}
+		else {
+			for (int i = 0; i < currentSize; i++)
+			{
+				cout << data[i] << ' ';
+			}
+			cout << endl;
+		}
+	}
+
+	// phần tử có giá trị lớn nhất trong heap
+	T const front() {
+		if (currentSize >= 0) {
+			return data[0];
+		}
+		else {
+			return T();
+		}
+	}
+
+	T pop() {
+		if (currentSize > 0) {
+			T removed = data[0];
+			data[0] = data[currentSize - 1];
+			currentSize--;
+			siftDown(0);
+			return removed;
+		}
+		else {
+			throw exception("Hang doi rong.");
+		}
+	}
+
+	// hàm sàng xuống
+	void siftDown(int index) {
+		int largest = index;
+		int left = 2 * index + 1;
+		int right = 2 * index + 2;
+		if (left < currentSize && data[left] > data[largest]) {
+			largest = left;
+		}
+		if (right < currentSize && data[right] > data[largest]) {
+			largest = right;
+		}
+		if (largest != index) {
+			swap(data[index], data[largest]);
+			siftDown(largest);
+		}
+	}
+
+	int size() {
+		return currentSize;
+	}
+
+	bool isEmpty() {
+		return currentSize == 0;
+	}
+
+	~PriorityQueue() {
+		delete[] data;
+	}
+};
+
+// quy ước giá trị nhỏ hơn sẽ có mức ưu tiên cao hơn
+int main() {
+	PriorityQueue<string> heap;
+	int choice;
+	do
+	{
+		cout << "================ OPTIONS ================\n";
+		cout << "1. Them moi mot node.\n";
+		cout << "2. Lay node dau tien trong queue.\n";
+		cout << "3. Xoa node dau khoi queue.\n";
+		cout << "4. Hien thi cac phan tu trong queue.\n";
+		cout << "0. Thoat chuong trinh.\n";
+		cout << "Lua chon cua ban? ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			cout << "<== The End ==>\n";
+			break;
+		case 1:
+		{
+			string x;
+			cout << "Nhap x = ";
+			cin >> x;
+			heap.push(x);
+			break;
+		}
+		case 2:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Gia tri node dau tien trong queue: " << heap.front() << endl;
+			}
+			break;
+		case 3:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Node vua xoa khoi queue: " << heap.pop() << endl;
+			}
+			break;
+		case 4:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Cac phan tu trong heap: \n";
+				heap.showElements();
+			}
+			break;
+		default:
+			cout << "Sai lua chon. Vui long thu lai.\n";
+			break;
+		}
+	} while (choice != 0);
+
+	return 0;
+}
+// ví dụ một max heap:
+//             90
+//           /    \
+//		   70     80
+//        / \    /  \
+//       40 30  10  50
+//      / \
+//     15  20
