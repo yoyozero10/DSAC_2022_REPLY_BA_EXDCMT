344c6bd9977eefcd33e77843e0532ce3e909846a
Trieu Than <trieuthan92pro@gmail.com>
2021-11-05 17:29:55 +0700

    Add exercises 3+4 l7.4

---
+++ Lesson7.4/Exercises3.cpp
@@ -0,0 +1,246 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.11
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// min heap template
+template<class T> class Heap {
+	T* data;
+	int currentSize;
+	int capacity;
+public:
+	Heap(int cap = 10) {
+		if (cap <= 0) {
+			capacity = 10;
+		}
+		else {
+			capacity = cap;
+		}
+		data = new T[capacity];
+		currentSize = 0;
+	}
+
+	bool add(T value) {
+		if (currentSize < capacity) {
+			data[currentSize] = value;
+			siftUp(currentSize);
+			currentSize++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void siftUp(int index) {
+		int parentIndex = (index - 1) / 2;
+		if (data[index] < data[parentIndex]) { // điều kiện tạo thành min heap
+			swap(data[index], data[parentIndex]);
+			siftUp(parentIndex);
+		}
+	}
+
+	void showElements() {
+		if (isEmpty()) {
+			cout << "Heap rong.\n";
+		}
+		else {
+			for (int i = 0; i < currentSize; i++)
+			{
+				cout << data[i] << ' ';
+			}
+			cout << endl;
+		}
+	}
+
+	int size() {
+		return currentSize;
+	}
+
+	bool isEmpty() {
+		return currentSize == 0;
+	}
+	// hàm xóa node khỏi heap
+	bool remove(T value) {
+		int index = findNodeIndex(value);
+		if (index >= 0) {
+			data[index] = data[currentSize - 1];
+			currentSize--;
+			siftDown(index);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	// hàm tìm node
+	int findNodeIndex(T value) {
+		for (int i = 0; i < currentSize; i++)
+		{
+			if (data[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	// hàm sàng xuống
+	void siftDown(int index) {
+		int largest = index;
+		int left = 2 * index + 1;
+		int right = 2 * index + 2;
+		if (left < currentSize && data[left] < data[largest]) {
+			largest = left;
+		}
+		if (right < currentSize && data[right] < data[largest]) {
+			largest = right;
+		}
+		if (largest != index) {
+			swap(data[index], data[largest]);
+			siftDown(largest);
+		}
+	}
+	// hàm trả về phần tử lớn nhất trong heap
+	T front() {
+		if (currentSize > 0) {
+			return data[0];
+		}
+		else {
+			return T();
+		}
+	}
+
+	// update node trong heap
+	bool update(T oldNode, T newNode) {
+		int index = findNodeIndex(oldNode);
+		if (index >= 0) {
+			data[index] = newNode;
+			int parentIndex = (index - 1) / 2;
+			if (data[parentIndex] > data[index]) {
+				siftUp(index);
+			}
+			else {
+				siftDown(index);
+			}
+			return true; // update success!
+		}
+		return false; // update failed
+	}
+	// hàm hủy
+	~Heap() {
+		delete[] data;
+	}
+};
+
+int main() {
+	Heap<int> heap;
+	int choice;
+	do
+	{
+		cout << "================ OPTIONS ================\n";
+		cout << "1. Them moi mot node.\n";
+		cout << "2. Hien thi cac phan tu trong heap.\n";
+		cout << "3. Tim gia tri x trong heap.\n";
+		cout << "4. Cap nhat node co gia tri x.\n";
+		cout << "5. Xoa node co gia tri x.\n";
+		cout << "6. Cho biet phan tu lon nhat trong heap.\n";
+		cout << "7. Cho biet kich thuoc cua heap.\n";
+		cout << "0. Thoat chuong trinh.\n";
+		cout << "Lua chon cua ban? ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 0:
+			cout << "<== The End ==>\n";
+			break;
+		case 1:
+		{
+			int x;
+			cout << "Nhap so nguyen x = ";
+			cin >> x;
+			heap.add(x);
+			break;
+		}
+		case 2:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				cout << "Cac phan tu trong heap: \n";
+				heap.showElements();
+			}
+			break;
+		case 3:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				int x;
+				cout << "Nhap gia tri can tim x = ";
+				cin >> x;
+				if (heap.findNodeIndex(x) >= 0) {
+					cout << "Gia tri " << x << " ton tai trong heap.\n";
+				}
+				else {
+					cout << x << " khong ton tai trong heap.\n";
+				}
+			}
+			break;
+		case 4:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				int x, newX;
+				cout << "Nhap gia tri can update x = ";
+				cin >> x;
+				cout << "Gia tri moi cua x = ";
+				cin >> newX;
+				if (heap.update(x, newX)) {
+					cout << "Cap nhat thanh cong thanh cong.\n";
+				}
+				else {
+					cout << "Xoa that bai.\n";
+				}
+			}
+			break;
+		case 5:
+			if (heap.isEmpty()) {
+				cout << "Heap rong.\n";
+			}
+			else {
+				int x;
+				cout << "Nhap gia tri can xoa x = ";
+				cin >> x;
+				if (heap.remove(x)) {
+					cout << "Xoa thanh cong.\n";
+				}
+				else {
+					cout << "Xoa that bai.\n";
+				}
+			}
+			break;
+		case 6:
+			cout << "Phan tu nho nhat trong heap: " << heap.front() << endl;
+			break;
+		case 7:
+			cout << "Kich thuoc heap: " << heap.size() << endl;
+			break;
+		default:
+			cout << "Sai lua chon. Vui long thu lai.\n";
+			break;
+		}
+	} while (choice != 0);
+	return 0;
+}
+
+//             90
+//           /    \
+//		   70     80
+//        / \    /  \
+//       40 30  10  50
+//      / \
+//     15  20
