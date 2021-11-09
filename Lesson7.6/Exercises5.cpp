d5b3520efccb6346c482033d09ca8fc42681b150
Trieu Than <trieuthan92pro@gmail.com>
2021-11-09 15:54:27 +0700

    Add ex5+6 lesson 7.6

---
+++ Lesson7.6/Exercises5.cpp
@@ -0,0 +1,73 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.11
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// hàm sàng xuống
+template<class T> void siftDown(T* arr, int n, int index) {
+	int largest = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
+	if (left < n && arr[left] > arr[largest]) {
+		largest = left;
+	}
+	if (right < n && arr[right] > arr[largest]) {
+		largest = right;
+	}
+	if (largest != index) {
+		swap(arr[index], arr[largest]);
+		siftDown(arr, n, largest);
+	}
+}
+
+// hàm hiển thị các phần tử mảng
+template<class T> void showElements(T* arr, int n) {
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+}
+
+// hàm sắp xếp heap sort
+template<class T> void heapSort(T* arr, int n) {
+	for (int i = (n / 2) - 1; i >= 0; i--)
+	{
+		siftDown(arr, n, i);
+	}
+	for (int i = n - 1; i > 0; i--)
+	{
+		swap(arr[0], arr[i]);
+		showElements(arr, n); // hiển thị kết quả
+		siftDown(arr, i, 0);
+	}
+}
+
+// hàm đọc dữ liệu đầu vào
+template<class T> void readInput(T* arr, int n) {
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+int main() {
+	int testNumber = 1;
+	int t; // số bộ test
+	int n;
+	cin >> t; // đọc số bộ test
+	while (t--) { // khi t > 0
+		cin >> n; // đọc số phần tử của mảng
+		int* arr = new int[n];
+		readInput(arr, n); // đọc các phần tử trong mảng
+		heapSort(arr, n); // sắp xếp 
+		cout << "Test " << testNumber++ << ":\n"; // hiển thị test thứ mấy
+		delete[] arr; // thu hồi bộ nhớ cấp phát động
+	}
+
+	return 0;
+}
---
+++ Lesson7.6/Exercises6.cpp
@@ -0,0 +1,187 @@
+﻿/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.11
+ *
+ */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+// hàm sàng xuống
+template<class T> void siftDown(T* arr, int n, int index) {
+	int largest = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
+	if (left < n && arr[left] > arr[largest]) {
+		largest = left;
+	}
+	if (right < n && arr[right] > arr[largest]) {
+		largest = right;
+	}
+	if (largest != index) {
+		swap(arr[index], arr[largest]);
+		siftDown(arr, n, largest);
+	}
+}
+
+// hàm hiển thị các phần tử mảng
+template<class T> void showElements(T* arr, int n) {
+	cout << left << setw(12) << "Ma NV" << setw(10) << "Ho"
+		<< setw(12) << "Dem" << setw(10) << "Ten"
+		<< setw(12) << "Luong" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i];
+	}
+	cout << endl;
+}
+
+// hàm sắp xếp heap sort
+template<class T> void heapSort(T* arr, int n) {
+	for (int i = (n / 2) - 1; i >= 0; i--)
+	{
+		siftDown(arr, n, i);
+	}
+	for (int i = n - 1; i > 0; i--)
+	{
+		swap(arr[0], arr[i]);
+		siftDown(arr, i, 0);
+	}
+}
+
+class Employee {
+	friend ostream& operator << (ostream&, const Employee&);
+private:
+	string id;
+	string first;
+	string last;
+	string mid;
+	long salary;
+public:
+	Employee(string id = "", string first = "", string last = "",
+		string mid = "", long salary = 0) {
+		setId(id);
+		setFirst(first);
+		setLast(last);
+		setMid(mid);
+		setSalary(salary);
+	}
+
+	void setId(string id) {
+		this->id = id;
+	}
+
+	void setFirst(string first) {
+		this->first = first;
+	}
+
+	void setMid(string mid) {
+		this->mid = mid;
+	}
+
+	void setLast(string last) {
+		this->last = last;
+	}
+
+	void setSalary(long salary) {
+		this->salary = salary;
+	}
+
+	string getId() {
+		return id;
+	}
+
+	string getFirst() {
+		return first;
+	}
+
+	string getLast() {
+		return last;
+	}
+
+	string getMid() {
+		return mid;
+	}
+
+	long getSalary() {
+		return salary;
+	}
+
+	bool operator > (const Employee& emp) {
+		if (salary > emp.salary) {
+			return true;
+		}
+		else if (salary == emp.salary) {
+			if (first.compare(emp.first) < 0) {
+				return true;
+			}
+			else if (first.compare(emp.first) == 0) {
+				if (last.compare(emp.last) < 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+};
+
+ostream& operator << (ostream& os, const Employee& e) {
+	os << left << setw(12) << e.id << setw(10) << e.last
+		<< setw(12) << e.mid << setw(10) << e.first
+		<< setw(12) << e.salary << endl;
+	return os;
+}
+
+// hàm đọc dữ liệu đầu vào
+void createEmployee(string str, Employee& e) {
+	stringstream ss(str);
+	string data[10];
+	string s;
+	int size = 0; // biến đếm số từ tách được từ string
+	while (ss >> s) {
+		data[size++] = s;
+	}
+	string id = data[0];
+	string first = data[size - 2], last = data[1], mid;
+	for (int j = 2; j < size - 2; j++)
+	{
+		mid.append(data[j]);
+		if (j < size - 3) {
+			mid.append(" ");
+		}
+	}
+	long salary = stol(data[size - 1]);
+	e.setFirst(first);
+	e.setLast(last);
+	e.setMid(mid);
+	e.setId(id);
+	e.setSalary(salary);
+}
+
+int main() {
+	int t; // số bộ test
+	ifstream ifs("INPUT.DAT");
+	if (ifs) {
+		ifs >> t; // đọc số bộ test
+		ifs.ignore();
+		Employee* arr = new Employee[t];
+		for (int i = 0; i < t; i++) {
+			string str;
+			getline(ifs, str);
+			createEmployee(str, arr[i]);
+		}
+		ifs.close();
+		heapSort(arr, t); // sắp xếp 
+		showElements(arr, t);
+		delete[] arr; // thu hồi bộ nhớ cấp phát động
+	}
+	else {
+		cout << "<== Khong mo duoc file. Vui long kiem tra lai! ==>\n";
+	}
+
+	return 0;
+}
