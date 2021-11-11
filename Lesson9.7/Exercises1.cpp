c460aad76e18cfdc8136fae375656acbaf219ae9
Trieu Than <trieuthan92pro@gmail.com>
2021-11-12 00:13:38 +0700

    Add exercises l9.7

---
+++ Lesson9.7/Exercises1.cpp
@@ -0,0 +1,63 @@
+﻿/**
+*	@author:	Branium Academy
+*	@version:	2021.11
+*	@see:		https://braniumacademy.net
+*/
+
+#include <iostream>
+using namespace std;
+
+// thuật toán sắp xếp và phân mảnh
+template<class T> int partition(T* arr, int left, int right) {
+	T pivot = arr[right];
+	int i = left;
+	for (int j = left; j <= right; j++)
+	{
+		if (arr[j] < pivot) {
+			swap(arr[i], arr[j]);
+			i++;
+		}
+	}
+	swap(arr[i], arr[right]);
+	return i;
+}
+
+// thuật toán sắp xếp nhanh
+template<class T> void quickSort(T* arr, int left, int right) {
+	if (left < right) {
+		int p = partition(arr, left, right);
+		quickSort(arr, left, p - 1);
+		quickSort(arr, p + 1, right);
+	}
+}
+
+template<class T> void showArrayElements(T* arr, size_t size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+template<class T> void readData(T* arr, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; i++) {
+		int n;
+		cin >> n;
+		int* arr = new int[n];
+		readData(arr, n);
+		quickSort(arr, 0, n-1);
+		cout << "Test " << i << ":\n";
+		showArrayElements(arr, n);
+		delete[] arr;
+	}
+	return 0;
+}
---
+++ Lesson9.7/Exercises4.cpp
@@ -0,0 +1,69 @@
+﻿/**
+*	@author:	Branium Academy
+*	@version:	2021.11
+*	@see:		https://braniumacademy.net
+*/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+// thuật toán sắp xếp và phân mảnh
+template<class T> int partition(T* arr, int left, int right) {
+	T pivot = arr[right];
+	int i = left;
+	for (int j = left; j <= right; j++)
+	{
+		if (arr[j] > pivot) {
+			swap(arr[i], arr[j]);
+			i++;
+		}
+	}
+	swap(arr[i], arr[right]);
+	return i;
+}
+
+// thuật toán sắp xếp nhanh
+template<class T> void quickSort(T* arr, int left, int right) {
+	if (left < right) {
+		int p = partition(arr, left, right);
+		quickSort(arr, left, p - 1);
+		quickSort(arr, p + 1, right);
+	}
+}
+
+// hàm hiển thị các phần tử mảng bất kì
+template<class T> void showArrayElements(T* arr, size_t size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+// hàm tách từ trong chuỗi string
+void split(string str, string* data, int& size) {
+	stringstream ss(str);
+	string s;
+	while (ss >> s) {
+		data[size++] = s;
+	}
+}
+
+int main() {
+	int t;
+	cin >> t;
+	cin.ignore(); // loại bỏ bộ đệm
+	for (int i = 1; i <= t; i++) {
+		string arr[500];
+		int n = 0;
+		string inputStr;
+		getline(cin, inputStr);
+		split(inputStr, arr, n);
+		quickSort(arr, 0, n - 1);
+		cout << "Test " << i << ":\n";
+		showArrayElements(arr, n);
+	}
+	return 0;
+}
